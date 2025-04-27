d70b09c7c935d28994d2a91cecaf0b39d6151e79
Ravi Prasad <Raviprasad53339@gmail.com>
2025-04-27 08:49:18 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,13 +1,13 @@
-client_id = 'Q7TT1GLLUH-100'
-secret_key = 'IE7ISPCG2I'
+client_id = 'Q7TT1GLXGL-500'
+secret_key = 'IE7IMBGOP2I'
 redirect_uri = 'https://www.google.com/'
 
-user_name='XR29131'
-totp_key='QHZESTDG7SLU543UDIPALCR3MKF7H7YQ'
-pin1 = "9"
+user_name='BJ456793'
+totp_key='QHZESTDG7S57GFCVI74JNGDP'
+pin1 = "1"
 pin2 = "6"
-pin3 = "5"
-pin4 = "3"
+pin3 = "6"
+pin4 = "7"
 
 import pyotp as tp
 t = tp.TOTP(totp_key).now()
